5e9a08ce5d63e6c12ff236a7a8db68a26a372120
OkamiAka <119871897+OkamiAka@users.noreply.github.com>
2023-04-03 11:22:12 +0200

    2 files modified

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -5,7 +5,15 @@ function MenuList({ foodItems }) {
   console.log(foodItems);
   return (
     <div>
-      {/*render a MenuItem component to each element of the props array*/}
+      {foodItems.map((item) => (
+        <MenuItem
+          description={item.description}
+          foodImage={item.foodImage}
+          isFavorite={item.isFavorite}
+          itemName={item.itemName}
+          price={item.price}
+        />
+      ))}
     </div>
   );
 }
